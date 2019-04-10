25455a27165a7d5c21c61cb09737afe89fd63f56
Allison Collins <allisonkatecollins@gmail.com>
2019-04-10 16:04:06 -0500

    working commit

--- BangazonWorkforce/Models/Department.cs
+++ BangazonWorkforce/Models/Department.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,8 +8,12 @@ namespace BangazonWorkforce.Models
 {
     public class Department
     {
+        [Required]
         public int Id { get; set; }
+        [Required]
+        [MaxLength(55)]
         public string Name { get; set; }
+        [Required]
         public int Budget { get; set; }
         public List<Employee> EmployeeList { get; set; } = new List<Employee>();
     }
