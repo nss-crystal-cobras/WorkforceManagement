2481cd9df687fd67225ec8e43588c23f5fc798a2
Daniel Brewer <danielbrewer2014@comcast.net>
2019-04-10 14:22:15 -0500

    employee list is displaying

--- BangazonWorkforce/Models/Employee.cs
+++ BangazonWorkforce/Models/Employee.cs
@@ -10,7 +10,7 @@ namespace BangazonWorkforce.Models
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public bool IsSupervisor { get; set; }
+        public bool IsSuperVisor { get; set; }
         public int DepartmentId { get; set; }
         public Department Department { get; set; }
         public Computer Computer { get; set; } = new Computer();
