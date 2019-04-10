ff2b072faed7fffda4fc4cbdba35448cb86b0fb3
Allison Collins <allisonkatecollins@gmail.com>
2019-04-10 15:23:52 -0500

    added annotations to employee model

--- BangazonWorkforce/Models/Employee.cs
+++ BangazonWorkforce/Models/Employee.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,10 +8,17 @@ namespace BangazonWorkforce.Models
 {
     public class Employee
     {
+        [Required]
         public int Id { get; set; }
+        [Required]
+        [MaxLength(55)]
         public string FirstName { get; set; }
+        [Required]
+        [MaxLength(55)]
         public string LastName { get; set; }
+        [Required]
         public bool IsSupervisor { get; set; }
+        [Required]
         public int DepartmentId { get; set; }
         public Department Department { get; set; }
         public Computer Computer { get; set; } = new Computer();
