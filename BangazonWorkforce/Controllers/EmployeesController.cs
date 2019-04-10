2880fef129a56b4ff38bd70c424e5a36a618a53c
loetek <jdwheeler0@gmail.com>
2019-04-10 14:50:30 -0500

    Working on edit.

--- BangazonWorkforce/Controllers/EmployeesController.cs
+++ BangazonWorkforce/Controllers/EmployeesController.cs
@@ -1,14 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
+using BangazonWorkforce.Models;
+using BangazonWorkforce.Models.ViewModels;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 
 namespace BangazonWorkforce.Controllers
 {
     public class EmployeesController : Controller
     {
+        private readonly IConfiguration _configuration;
+
+        public EmployeesController(IConfiguration configuration)
+        {
+            this._configuration = configuration;
+        }
+
+        public SqlConnection Connection
+        {
+
+            get { return new SqlConnection(_configuration.GetConnectionString("DefaultConnection")); }
+        }
+
         // GET: Employees
         public ActionResult Index()
         {
@@ -47,23 +64,56 @@ namespace BangazonWorkforce.Controllers
         // GET: Employees/Edit/5
         public ActionResult Edit(int id)
         {
-            return View();
+            Employee employee = GetEmployeeById(id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            //This piece of code keeps the type correct to pass into the edit
+            EmployeeEditViewModel viewModel = new EmployeeEditViewModel
+            {
+                Departments = GetAllDepartments(),
+                Employee = employee
+            };
+
+            return View(viewModel);
         }
 
         // POST: Employees/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
+        public ActionResult Edit(int id, EmployeeEditViewModel viewModel)
         {
             try
             {
-                // TODO: Add update logic here
+                using (SqlConnection conn = Connection)
+                {
+                    conn.Open();
+                    using (SqlCommand cmd = conn.CreateCommand())
+                    {
+                        cmd.CommandText = @"UPDATE employee 
+                                           SET firstname = @firstname, 
+                                               lastname = @lastname,
+                                               isSuper = @isSupervisor, 
+                                               deptid = @departmentId
+                                         WHERE id = @id;";
 
-                return RedirectToAction(nameof(Index));
+                        cmd.Parameters.Add(new SqlParameter("@firstname", viewModel.Employee.FirstName));
+                        cmd.Parameters.Add(new SqlParameter("@lastname", viewModel.Employee.LastName));
+                        cmd.Parameters.Add(new SqlParameter("@isSupervisor", viewModel.Employee.IsSupervisor));
+                        cmd.Parameters.Add(new SqlParameter("@departmentId", viewModel.Employee.DepartmentId));
+                        cmd.Parameters.Add(new SqlParameter("@id", id));
+
+                        cmd.ExecuteNonQuery();
+
+                        return RedirectToAction(nameof(Index));
+                    }
+                }
             }
             catch
             {
-                return View();
+                return View(viewModel);
             }
         }
 
@@ -89,5 +139,89 @@ namespace BangazonWorkforce.Controllers
                 return View();
             }
         }
+
+
+        // JD created to grab individual items for editing.
+
+        private Employee GetEmployeeById(int id)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"SELECT e.Id AS EmployeeId,
+                                               e.FirstName, 
+                                               e.LastName, 
+                                               e.IsSuper 
+                                               e.DeptId,
+                                               e.Name AS DeptName
+                                         FROM Employee e LEFT JOIN  d on e.departmentid = d.id
+                                         WHERE  e.Id = @id";
+                    cmd.Parameters.Add(new SqlParameter("@id", id));
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    Employee employee = null;
+
+                    if (reader.Read())
+                    {
+                        employee = new Employee
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("EmployeeId")),
+                            FirstName = reader.GetString(reader.GetOrdinal("FirstName")),
+                            LastName = reader.GetString(reader.GetOrdinal("LastName")),
+                            IsSupervisor = reader.GetBoolean(reader.GetOrdinal("IsSuper")),
+                            DepartmentId = reader.GetInt32(reader.GetOrdinal("DeptId")),
+                            Department = new Department
+                            {
+                                Id = reader.GetInt32(reader.GetOrdinal("CohortId")),
+                                Name = reader.GetString(reader.GetOrdinal("CohortName")),
+                            }
+                        };
+                    }
+
+                    reader.Close();
+
+                    return (employee) ;
+
+                }
+            }
+        }
+
+        // JD - Wrote this for grabbing all instances of departments for employee views.
+        private List<Department> GetAllDepartments()
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"SELECT id, name from Department;";
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    List<Department> departments = new List<Department>();
+
+                    while (reader.Read())
+                    {
+                        departments.Add(new Department
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            Name = reader.GetString(reader.GetOrdinal("name"))
+                        });
+                    }
+
+                    reader.Close();
+
+                    return departments;
+                }
+            }
+
+        }
+
+
+
+
+
     }
 }
+
---
+++ BangazonWorkforce/Models/ViewModels/EmployeeEditViewModel.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BangazonWorkforce.Models.ViewModels
+{
+
+    //JD -- This model will allow the editing of the employee with a dropdown.
+
+    public class EmployeeEditViewModel
+    {
+
+        public Employee Employee { get; set; }
+        public Department Department { get; set; }
+        public List<Department> Departments { get; set; }
+        public List<SelectListItem> DepartmentOptions
+        {
+            get
+            {
+                if (Departments == null)
+                {
+                    return null;
+                }
+
+                return Departments.Select(d => new SelectListItem
+                {
+                    Value = d.Id.ToString(),
+                    Text = d.Name
+                }).ToList();
+            }
+        }
+
+    }
+}
