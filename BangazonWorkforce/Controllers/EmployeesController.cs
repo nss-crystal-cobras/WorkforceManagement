6ce80499081de332bb74c5532d98c2400f598d52
hanna <hannahmneal@gmail.com>
2019-04-11 13:36:36 -0500

    commented out employee details

--- BangazonWorkforce/Controllers/EmployeesController.cs
+++ BangazonWorkforce/Controllers/EmployeesController.cs
@@ -73,106 +73,102 @@ namespace BangazonWorkforce.Controllers
 //        4. Training programs they have attended, or plan on attending (access the list of training programs associated with the employee)
 
         // GET: Employees/Details/{id}
-        public ActionResult Details(int id)
-        {
-            using (SqlConnection conn = Connection)
-            {
-                conn.Open();
-                using (SqlCommand cmd = conn.CreateCommand())
-                {
-                    cmd.CommandText = @"SELECT
-	                                        e.Id AS 'Employee Id',
-	                                        e.FirstName AS 'Employee First Name',
-	                                        e.LastName AS 'Employee Last Name',
-	                                        d.[Name] AS 'Department Name',
-	                                        ce.AssignDate AS 'Computer Assigned On',
-	                                        c.Id AS 'Computer Id',
-	                                        c.Make AS 'Computer Make',
-	                                        c.Manufacturer AS 'Computer Manufacturer',
-	                                        et.TrainingProgramId AS 'Training Program Id', 
-	                                        tp.[Name] AS 'Training Program', 
-	                                        tp.StartDate AS 'Training Program Start'
-                                    FROM Employee AS e 
-                                    LEFT JOIN Department d ON d.Id = e.DepartmentId
-                                    RIGHT JOIN ComputerEmployee ce ON ce.EmployeeId = e.Id 
-                                    INNER JOIN Computer c ON c.Id = ce.ComputerId
-                                    RIGHT JOIN EmployeeTraining et ON et.EmployeeId = e.Id
-                                    INNER JOIN TrainingProgram tp ON tp.Id = et.TrainingProgramId
-                                    WHERE e.Id = @id";
+        //public ActionResult Details(int id)
+        //{
+        //    using (SqlConnection conn = Connection)
+        //    {
+        //        conn.Open();
+        //        using (SqlCommand cmd = conn.CreateCommand())
+        //        {
+        //            cmd.CommandText = @"SELECT
+	       //                                 e.Id AS 'Employee Id',
+	       //                                 e.FirstName AS 'Employee First Name',
+	       //                                 e.LastName AS 'Employee Last Name',
+        //                                    d.Id AS 'Department Id',
+	       //                                 d.[Name] AS 'Department Name',
+	       //                                 ce.AssignDate AS 'Computer Assigned On',
+	       //                                 c.Id AS 'Computer Id',
+	       //                                 c.Make AS 'Computer Make',
+	       //                                 c.Manufacturer AS 'Computer Manufacturer',
+	       //                                 et.TrainingProgramId AS 'Training Program Id', 
+	       //                                 tp.[Name] AS 'Training Program', 
+	       //                                 tp.StartDate AS 'Training Program Start'
+        //                            FROM Employee AS e 
+        //                            LEFT JOIN Department d ON d.Id = e.DepartmentId
+        //                            RIGHT JOIN ComputerEmployee ce ON ce.EmployeeId = e.Id 
+        //                            INNER JOIN Computer c ON c.Id = ce.ComputerId
+        //                            RIGHT JOIN EmployeeTraining et ON et.EmployeeId = e.Id
+        //                            INNER JOIN TrainingProgram tp ON tp.Id = et.TrainingProgramId
+        //                            WHERE e.Id = @id";
 
-                    //NOTE: HMN: This query was tested in SQL and produced, overall, the desired results (based on issue ticket specifications) The List of training programs for employees (past and future) may need to be tweaked to show end date or past date, however.
+        //            //NOTE: HMN: This query was tested in SQL and produced, overall, the desired results (based on issue ticket specifications) The List of training programs for employees (past and future) may need to be tweaked to show end date or past date, however.
 
-                    cmd.Parameters.Add(new SqlParameter("@id", id));
-                    SqlDataReader reader = cmd.ExecuteReader();
+        //            cmd.Parameters.Add(new SqlParameter("@id", id));
+        //            SqlDataReader reader = cmd.ExecuteReader();
 
 
-                    //If employee = null, it doesn't exist; create an object for it.
-                    //An employee must have a department they work in (i.e., dept cannot be null).
-                    //An employee does not have to be enrolled in a training program but if they are, the training program type needs to show all past and future training programs for the employee.
-                    //An employee must have a computer (i.e., computer !== null).
+        //            //If employee = null, it doesn't exist; create an object for it.
+        //            //An employee must have a department they work in (i.e., dept cannot be null).
+        //            //An employee must have a computer (i.e., computer !== null).
+        //            //An employee does not have to be enrolled in a training program but if they are, the training program type needs to show all past and future training programs for the employee.
 
-                    Dictionary<int, Employee> employees = new Dictionary<int, Employee>();
+        //                while (reader.Read())
+        //                {
+        //                    if (employee == null)
+        //                    {
+        //                        employee = new Employee
+        //                        {
+        //                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+        //                            FirstName = reader.GetString(reader.GetOrdinal("Employee First Name")),
+        //                            LastName = reader.GetString(reader.GetOrdinal("Employee Last Name")),
+        //                            Department = new Department
+        //                            {
+        //                                Id = reader.GetInt32(reader.GetOrdinal("Department Id")),
+        //                                Name = reader.GetString(reader.GetOrdinal("Department Name")),
+        //                            },
+        //                            Computer = new Computer()
+        //                            {
+        //                                Id = reader.GetInt32(reader.GetOrdinal("Id")),
+        //                                Make = reader.GetString(reader.GetOrdinal("Make")),
+        //                                Manufacturer = reader.GetString(reader.GetOrdinal("Manufacturer"))
+        //                            },
+        //                            TrainingProgramList = new List<TrainingProgram>()
+        //                            {
 
-                        while (reader.Read())
-                        {
-                            int employeeid = reader.GetInt32(reader.GetOrdinal("Id"));
+        //                            }
+        //                        }
+        //                        employees.Add(id, employee);
+        //                    };
+        //                    if (!reader.IsDBNull(reader.GetOrdinal("Employee Id")))
+        //                    {
+                                
 
-                            if (!employees.ContainsKey(employeeid))
-                            {
 
-                                Employee employee = new Employee
-                                {
-                                    Id = reader.GetInt32(reader.GetOrdinal("Id")),
-                                    FirstName = reader.GetString(reader.GetOrdinal("Employee First Name")),
-                                    LastName = reader.GetString(reader.GetOrdinal("Employee Last Name")),
-                                    TrainingProgramList = new List<TrainingProgram>(),
-                                    Computer = new Computer(),
-                                    Department = new Department
-                                    {
-                                        Id = reader.GetInt32(reader.GetOrdinal("Department Id")),
-                                        Name = reader.GetString(reader.GetOrdinal("Department Name")),
-                                    },
-                                };
-                                employees.Add(employeeid, employee);
-                            };
-                            if (!reader.IsDBNull(reader.GetOrdinal("ecId")))
-                            {
-                                Employee currentemployee = employees[employeeid];
-                                currentemployee.Computer = new Computer
-                                {
-                                    Make = reader.GetString(reader.GetOrdinal("Make")),
-                                    Manufacturer = reader.GetString(reader.GetOrdinal("Manufacturer")),
-                                };
-                            }
-                            if (!reader.IsDBNull(reader.GetOrdinal("etId")))
-                            {
-                                Employee currentemployee = employees[employeeid];
+        //                        {
+        //                            Make = reader.GetString(reader.GetOrdinal("Make")),
+        //                            Manufacturer = reader.GetString(reader.GetOrdinal("Manufacturer")),
+        //                        };
+        //                    }
+        //                    if (!reader.IsDBNull(reader.GetOrdinal("Id")))
+        //                    {
+        //                        if (!currentemployee.TrainingProgramList.Any(x => x.Id == reader.GetInt32(reader.GetOrdinal("Id"))))
+        //                        {
+        //                            currentemployee.TrainingProgramList.Add(new TrainingProgram
+        //                            {
+        //                                Id = reader.GetInt32(reader.GetOrdinal("Id")),
+        //                                Name = reader.GetString(reader.GetOrdinal("Training Program Name")),
+        //                                StartDate = reader.GetDateTime(reader.GetOrdinal("Training Program Start Date")),
+        //                                EndDate = reader.GetDateTime(reader.GetOrdinal("Training Program EndDate"))
+        //                            });
+        //                        }
+        //                    }
+        //                }
 
-                                if (!currentemployee.TrainingProgramList.Any(x => x.Id == reader.GetInt32(reader.GetOrdinal("tpId"))))
-                                {
-                                    currentemployee.TrainingProgramList.Add(new TrainingProgram
-                                    {
-                                        Id = reader.GetInt32(reader.GetOrdinal("tpId")),
-                                        Name = reader.GetString(reader.GetOrdinal("tpName")),
-                                        StartDate = reader.GetDateTime(reader.GetOrdinal("StartDate")),
-                                        EndDate = reader.GetDateTime(reader.GetOrdinal("EndDate"))
-                                    });
-                                }
-                            }
-                        }
-
-                        reader.Close();
-
-                        List<Employee> employeeDetail = employees.Values.ToList();
-
-                        foreach (Employee e in employeeDetail)
-                        {
-                            return View(e);
-                        }
-                        return View();
-                    }
-                }
-            }
+        //                reader.Close();
+        //                return View();
+        //            }
+        //        }
+        //    }
             
 
 
